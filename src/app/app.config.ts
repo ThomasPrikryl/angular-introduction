bbda7ab73a4d274565504e10852004fe7a7d568f
Thomas Prikryl <45074513+ThomasPrikryl@users.noreply.github.com>
2024-06-02 16:53:58 +0200

    [app-100a]

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { FormsModule } from '@angular/forms';
+import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideHttpClient(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     importProvidersFrom(FormsModule),
   ]
 };
